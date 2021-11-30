3ecacfa43fe7d043e7f36752c0f38ac057c01e3a
paaaarv <pkumar@trinity.edu>
2021-11-30 16:06:20 -0600

    sorting algorithm

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 
+
+function findMinAndRemoveSorted(array){
+    let min=array[0];
+    let index=0
+    for(let i=0; i<array.length; i++){
+        if (array[i]<min){
+            min=array[i]
+            index=i
+        }
+    }
+    array.splice(index,1)
+    return min;
+}
+
+
+function merge(firstArray,secondArray){
+    let sorted=[];
+    let currentMin;
+    while(firstArray.length !=0 && secondArray.length !=0){
+        if(firstArray[0]<=secondArray[0]){
+            currentMin=findMinAndRemoveSorted(firstArray)
+            sorted.push(currentMin)
+        }else{
+            let currentMin=findMinAndRemoveSorted(secondArray)
+            sorted.push(currentMin)
+        }
+}
+    return sorted.concat(firstArray).concat(secondArray);
+
+}
+
+function mergeSort(array){
+    let midpoint=array.length/2
+    let firstHalf=array.slice(0,midpoint)
+    let secondHalf=array.slice(midpoint,array.length)
+    if(array.length <2){
+	  return array
+	} else {
+	     return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+	}
+}
